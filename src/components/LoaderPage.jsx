a2c21f742e7ae9257c5faabb7239bbfbdff29292
sabbCodes <sarafaabbas@gmail.com>
2024-06-09 20:17:13 +0100

    loader

--- src/components/LoaderPage.jsx
+++ src/components/LoaderPage.jsx
@@ -1,11 +1,11 @@
-import BookImg from '/Book.png'
+import BookImg from '/Book.png';
 
 function LoaderPage() {
-    return(
+    return (
         <main className='bg-white h-screen w-screen flex items-center justify-center'>
-            <img src={BookImg} />
+            <img src={BookImg} alt="Loading..." className='max-w-full max-h-full' />
         </main>
-    )
+    );
 }
 
 export default LoaderPage;
